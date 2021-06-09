3b65b053a89f2be12369a47342e846e8b19fe7f5
Vinicius <denis.vini@hotmail.com>
2021-06-09 23:01:36 +0100

    Adicionar arquivos de projeto.

---
+++ Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.Models
+{
+    public class Pokemon
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Type { get; set; }
+        public List<String> Effective { get; set; }
+
+    }
+}
